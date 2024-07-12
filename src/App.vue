82f39df042b03474e2697f411af2dc69918d02d3
ITPRESCO\kk.kuo <kk.kuo@presco.ws>
2024-07-12 16:17:40 +0800

    init build project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <HeaderBar />
+  </section>
+</template>
+<script setup>
+import { Settings } from 'lucide-vue-next';
+import HeaderBar from '@/components/HeaderBar.vue';
+</script>
+
+<style></style>
---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="w-full bg-[#53b478] py-6 px-20 flex justify-between items-center">
+    <div class="text-white text-3xl font-bold">天氣概況</div>
+    <Settings class="w-8 h-8 text-white cursor-pointer" />
+  </div>
+</template>
+
+<script setup></script>
+
+<style></style>
